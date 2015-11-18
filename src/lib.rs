581f74cbec811cfc4b6b03131a65d9ee50fe1ec5
Paul Woolcock <paul@woolcock.us>
2015-11-17 22:44:57 -0500

    add clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! Ngrams
+//!
+//! Produce ngram sequences from inputs, and calculate the similarity
+//! between two items using their ngram similarity.
+
+#![deny(missing_docs,
+       missing_debug_implementations, missing_copy_implementations,
+       trivial_casts, trivial_numeric_casts,
+       unsafe_code,
+       unstable_features,
+       unused_import_braces, unused_qualifications)]
+#![cfg_attr(feature = "dev", allow(unstable_features))]
+#![cfg_attr(feature = "dev", feature(plugin))]
+#![cfg_attr(feature = "dev", plugin(clippy))]
+#![cfg_attr(feature = "dev", deny(clippy))]
+
+
 use std::fmt;
 use std::collections::{VecDeque};
 
