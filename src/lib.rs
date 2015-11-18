6bd481fdb0dd4847fe231d110d72585f815efbfa
Paul Woolcock <paul@woolcock.us>
2015-11-17 22:51:11 -0500

    mish-mash -- small implementation tweaks, little bit of documentation, and rustfmt pass

--- src/lib.rs
+++ src/lib.rs
@@ -16,230 +16,194 @@
 
 
 use std::fmt;
-use std::collections::{VecDeque};
+use std::collections::VecDeque;
 
-pub trait Pad {
-  fn symbol() -> Self;
+const WORD_SEP: &'static str = "\u{2060}";
 
-  fn len(n: u32) -> u32 { n - 1 }
-}
-
-impl<'a> Pad for &'a str {
-  fn symbol() -> Self {
-    "\u{2060}"
-  }
-}
-
-impl Pad for String {
-  fn symbol() -> Self {
-    "\u{2060}".to_string()
-  }
-}
-
-impl Pad for Vec<u8> {
-  fn symbol() -> Self {
-    "\u{2060}".to_string().into()
-  }
-}
-
-pub struct Padded<'a, T: 'a + Pad + fmt::Debug + Clone> {
-  source: Box<Iterator<Item=T> + 'a>,
-  len: u32,
-  symbol: T,
-  remaining: u32,
-  end: bool,
-}
-
-impl<'a, T: 'a + Pad + fmt::Debug + Clone> Padded<'a, T> {
-  pub fn new<U: 'a + Iterator<Item=T>>(source: U, n: u32) -> Padded<'a, T>{
-    let l = T::len(n);
-    Padded {
-      source: Box::new(source),
-      len: l,
-      symbol: T::symbol(),
-      remaining: l,
-      end: false,
-    }
-  }
-}
-
-impl<'a, T: 'a + Pad + fmt::Debug + Clone> Iterator for Padded<'a, T> {
-  type Item = T;
-
-  fn next(&mut self) -> Option<Self::Item> {
-    if self.remaining > 0 {
-      self.remaining = self.remaining - 1;
-      return Some(self.symbol.clone());
-    }
-
-    let result = self.source.next();
-
-    if result.is_none() {
-
-      if !self.end { // then this is the first time
-                     // we have seen this return None.
-        self.end = true;
-        self.remaining = self.len;
-      }
-
-      if self.remaining > 0 {
-        self.remaining = self.remaining - 1;
-        return Some(self.symbol.clone());
-      }
-
-    }
-
-    result
-  }
-}
-
+/// Main data type, implements the logic on splitting and grouping n-grams
 pub struct Ngrams<'a, T: 'a + Pad + fmt::Debug + Clone> {
-  source: Box<Iterator<Item=T> + 'a>,
-  num: u32,
-  memory: VecDeque<T>,
+    source: Box<Iterator<Item = T> + 'a>,
+    num: usize,
+    memory: VecDeque<T>,
+}
+
+impl<'a, T: 'a + Pad + fmt::Debug + Clone> fmt::Debug for Ngrams<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Ngrams(tokens, N)")
+    }
 }
 
 impl<'a, T: 'a + Pad + fmt::Debug + Clone> Ngrams<'a, T> {
-  pub fn new<V: 'a + Iterator<Item=T>>(source: V, num: u32) -> Ngrams<'a, T> {
-    Ngrams {
-      source: Box::new(Padded::new(source, num)),
-      num: num,
-      memory: VecDeque::with_capacity(num - 1 as usize),
+    /// The source for the `Ngrams` is expected to be pre-tokenized, this library
+    /// does not make any decisions regarding how your input should be tokenized.
+    pub fn new<V: 'a + Iterator<Item = T>>(source: V, n: usize) -> Ngrams<'a, T> {
+        Ngrams {
+            source: Box::new(Padded::new(source, n)),
+            num: n,
+            memory: VecDeque::with_capacity((n - 1)),
+        }
     }
-  }
 
-  pub fn pad_len(mut self, pad: u32) -> Ngrams<'a, T> {
-    self.source = Box::new(Padded::new(self.source, pad));
-    self
-  }
 }
 
 impl<'a, T: 'a + Pad + fmt::Debug + Clone> Iterator for Ngrams<'a, T> {
   type Item = Vec<T>;
 
-  fn next(&mut self) -> Option<Self::Item> {
-    // Fill the memory
-    while self.memory.len() < self.memory.capacity() {
-      // Can I unwrap here? I need to make sure that
-      // .next() can't return None before .memory is full
-      let a = self.source.next().unwrap();
-      self.memory.push_back(a);
-    }
-
-    let next_item = self.source.next();
-
-    match next_item {
-      None => return None,
-      Some(n) => {
-
-        let mut result = Vec::with_capacity(self.num as usize);
-
-        for elem in &self.memory {
-          result.push(elem.clone());
+    fn next(&mut self) -> Option<Self::Item> {
+        // Fill the memory
+        while self.memory.len() < self.memory.capacity() {
+            // Can I unwrap here? I need to make sure that
+            // .next() can't return None before .memory is full
+            let a = self.source.next().unwrap();
+            self.memory.push_back(a);
         }
 
-        result.push(n.clone());
+        let next_item = self.source.next();
 
-        let _ = self.memory.pop_front();
-        self.memory.push_back(n.clone());
+        match next_item {
+            None => None,
+            Some(n) => {
 
-        Some(result)
-      }
+                let mut result = Vec::with_capacity(self.num);
+
+                for elem in &self.memory {
+                    result.push(elem.clone());
+                }
+
+                result.push(n.clone());
+
+                let _ = self.memory.pop_front();
+                self.memory.push_back(n.clone());
+
+                Some(result)
+            }
+        }
     }
-  }
+}
+
+/// Implement this so `ngrams` knows how to pad the beginning and end of your input.
+///
+/// There are default implementations for `&str`, `String`, and `Vec<u8>`
+pub trait Pad {
+    /// The item returned from this method will be used to pad the beginning and end of each n-gram
+    fn symbol() -> Self;
+
+    /// Specifies how many characters of padding to add. Defaults to N - 1
+    fn len(n: usize) -> usize {
+        n - 1
+    }
+}
+
+impl<'a> Pad for &'a str {
+    fn symbol() -> Self {
+        WORD_SEP
+    }
+}
+
+impl Pad for String {
+    fn symbol() -> Self {
+        WORD_SEP.to_owned()
+    }
+}
+
+impl Pad for Vec<u8> {
+    fn symbol() -> Self {
+        WORD_SEP.to_owned().into()
+    }
+}
+
+struct Padded<'a, T: 'a + Pad + fmt::Debug + Clone> {
+    source: Box<Iterator<Item = T> + 'a>,
+    len: usize,
+    symbol: T,
+    remaining: usize,
+    end: bool,
+}
+
+impl<'a, T: 'a + Pad + fmt::Debug + Clone> Padded<'a, T> {
+    fn new<U: 'a + Iterator<Item = T>>(source: U, n: usize) -> Padded<'a, T> {
+        let l = T::len(n);
+        Padded {
+            source: Box::new(source),
+            len: l,
+            symbol: T::symbol(),
+            remaining: l,
+            end: false,
+        }
+    }
+}
+
+impl<'a, T: 'a + Pad + fmt::Debug + Clone> Iterator for Padded<'a, T> {
+  type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.remaining > 0 {
+            self.remaining = self.remaining - 1;
+            return Some(self.symbol.clone());
+        }
+
+        let result = self.source.next();
+
+        if result.is_none() {
+
+            if !self.end {
+                // then this is the first time
+                // we have seen this return None.
+                self.end = true;
+                self.remaining = self.len;
+            }
+
+            if self.remaining > 0 {
+                self.remaining = self.remaining - 1;
+                return Some(self.symbol.clone());
+            }
+
+        }
+
+        result
+    }
 }
 
 #[cfg(test)]
 mod tests {
 
-use super::{Ngrams, Pad};
-use std::string::ToString;
+    use super::Ngrams;
+    use std::string::ToString;
 
-#[test]
-fn test_words() {
-  let seq = "one two three four".split(' ');
-  let result: Vec<_> = Ngrams::new(seq, 2).collect();
-  assert_eq!(
-    result,
-    vec![
-      vec!["one", "two"],
-      vec!["two", "three"],
-      vec!["three", "four"],
-    ]
-  );
-}
+    #[test]
+    fn test_words() {
+        let seq = "one two three four".split(' ');
+        let result: Vec<_> = Ngrams::new(seq, 2).collect();
+        assert_eq!(result,
+                   vec![
+                vec!["\u{2060}", "one"],
+                vec!["one", "two"],
+                vec!["two", "three"],
+                vec!["three", "four"],
+                vec!["four", "\u{2060}"],
+            ]);
+    }
 
-#[test]
-fn test_chars() {
-  let seq = "test string".chars().map(|c| c.to_string());
-  let result: Vec<_> = Ngrams::new(seq, 4).collect();
-  assert_eq!(
-    result,
-    vec![
-      vec!["t", "e", "s", "t"],
-      vec!["e", "s", "t", " "],
-      vec!["s", "t", " ", "s"],
-      vec!["t", " ", "s", "t"],
-      vec![" ", "s", "t", "r"],
-      vec!["s", "t", "r", "i"],
-      vec!["t", "r", "i", "n"],
-      vec!["r", "i", "n", "g"],
-    ]
-  );
+    #[test]
+    fn test_chars() {
+        let seq = "test string".chars().map(|c| c.to_string());
+        let result: Vec<_> = Ngrams::new(seq, 4).collect();
+        assert_eq!(result,
+                   vec![
+                vec!["\u{2060}", "\u{2060}", "\u{2060}", "t"],
+                vec!["\u{2060}", "\u{2060}", "t", "e"],
+                vec!["\u{2060}", "t", "e", "s"],
+                vec!["t", "e", "s", "t"],
+                vec!["e", "s", "t", " "],
+                vec!["s", "t", " ", "s"],
+                vec!["t", " ", "s", "t"],
+                vec![" ", "s", "t", "r"],
+                vec!["s", "t", "r", "i"],
+                vec!["t", "r", "i", "n"],
+                vec!["r", "i", "n", "g"],
+                vec!["i", "n", "g", "\u{2060}"],
+                vec!["n", "g", "\u{2060}", "\u{2060}"],
+                vec!["g", "\u{2060}", "\u{2060}", "\u{2060}"],
+            ]);
+    }
 }
-
-/*
-#[test]
-fn test_words_wth_pad_left() {
-  let seq = "one two three four".split(' ');
-  let result: Vec<_> = Ngrams::new(seq, 2)
-                              .with_pad(Pad::Left("PAD"))
-                              .collect();
-  assert_eq!(
-    result,
-    vec![
-      vec!["PAD", "one"],
-      vec!["one", "two"],
-      vec!["two", "three"],
-      vec!["three", "four"],
-    ]
-  );
-}
-
-#[test]
-fn test_words_with_pad_right() {
-  let seq = "one two three four".split(' ');
-  let result: Vec<_> = Ngrams::new(seq, 2)
-                              .with_pad(Pad::Right("PAD"))
-                              .collect();
-  assert_eq!(
-    result,
-    vec![
-      vec!["one", "two"],
-      vec!["two", "three"],
-      vec!["three", "four"],
-      vec!["four", "PAD"],
-    ]
-  );
-}
-
-#[test]
-fn test_words_with_pad_both() {
-  let seq = "one two three four".split(' ');
-  let result: Vec<_> = Ngrams::new(seq, 2)
-                              .pad_len(1)
-                              .collect();
-  assert_eq!(
-    result,
-    vec![
-      vec!["PAD", "one"],
-      vec!["one", "two"],
-      vec!["two", "three"],
-      vec!["three", "four"],
-      vec!["four", "PAD"],
-    ]
-  );
-}
-*/
-}
-
